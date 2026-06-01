0f2b9602cdb8e1ca09b4d7d6dcb2076d9389e3ec
Samandar7185 <134359911+Samandar7185@users.noreply.github.com>
2026-06-01 20:29:12 +0500

    Added base model

---
+++ backend/app/models/base.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, DateTime
+from datetime import datetime
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
